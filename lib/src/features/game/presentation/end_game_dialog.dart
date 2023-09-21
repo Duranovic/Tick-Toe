90acc3d86d39c268a34728441d0bc5a73f235a12
Velid Duranovic <velid.duranovic@endava.com>
2023-09-22 01:12:22 +0200

    Implement new screen - End Game Dialog

--- lib/src/features/game/presentation/end_game_dialog.dart
+++ lib/src/features/game/presentation/end_game_dialog.dart
@@ -1,65 +1,148 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../../shared/domain/button_variations.dart';
+import '../../../shared/presentation/button.dart';
+import 'cubit/game_cubit.dart';
 
 class EndGameDialog extends StatefulWidget {
-  const EndGameDialog({super.key});
+  final GameCubit gameCubit;
+
+  const EndGameDialog({super.key, required this.gameCubit});
 
   @override
   _EndGameDialogState createState() => _EndGameDialogState();
 }
 
 class _EndGameDialogState extends State<EndGameDialog> {
-  int waitingTimeForRound = 5;
-  late Timer roundTimer;
-
   @override
   void initState() {
     super.initState();
-    roundTimer = Timer.periodic(const Duration(seconds: 1), (timer) {
-      if (waitingTimeForRound > 0) {
-        setState(() {
-          waitingTimeForRound--;
-        });
-      } else {
-        roundTimer.cancel();
-        Navigator.of(context).pop();
-      }
-    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Stack(
-      alignment: Alignment.bottomCenter,
+      alignment: Alignment.center,
       children: [
         // The transparent white background
         Container(
-          height: 300,
-          decoration: BoxDecoration(
-            color: Colors.white.withOpacity(1), // Adjust the opacity as needed
-          ),
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+            image: AssetImage("assets/images/background.jpg"),
+            fit: BoxFit.cover,
+          )),
         ),
         // The dialog box
         AlertDialog(
-          alignment: Alignment.bottomCenter,
+          alignment: Alignment.center,
+          insetPadding: EdgeInsets.zero,
+          contentPadding: EdgeInsets.zero,
+          shape: const BeveledRectangleBorder(
+            borderRadius: BorderRadius.zero,
+          ),
           backgroundColor:
-              Colors.transparent, // Make the background transparent
+              Colors.white.withOpacity(0), // Make the background transparent
           elevation: 0.0, // Remove the shadow behind the dialog content
 
           content: Column(
-            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              const Text(
-                'Game finished',
-                textAlign: TextAlign.center,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SvgPicture.asset(
+                    'assets/images/x.svg',
+                    width: 45,
+                    height: 45,
+                  ),
+                  const SizedBox(width: 20),
+                  const Text(
+                    'IS A WINNER',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                ],
               ),
-              Text(
-                waitingTimeForRound.toString(),
-                textAlign: TextAlign.center,
-              )
+              const SizedBox(height: 40),
+              Row(children: [
+                Expanded(
+                  child: Row(children: [
+                    const Icon(
+                      Icons.close_rounded,
+                      color: Color(0xff0972FF),
+                      size: 32,
+                    ),
+                    Text(
+                      "${widget.gameCubit.state.tickWins} wins",
+                      style: const TextStyle(
+                        fontSize: 14,
+                        decoration: TextDecoration.none,
+                        color: Color(0xff0972FF),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ]),
+                ),
+                const SizedBox(width: 10),
+                Expanded(
+                  child: Row(children: [
+                    const Icon(
+                      Icons.circle_outlined,
+                      color: Color(0xff09FFD6),
+                      size: 32,
+                    ),
+                    Text(
+                      "${widget.gameCubit.state.toeWins} wins",
+                      style: const TextStyle(
+                        fontSize: 14,
+                        decoration: TextDecoration.none,
+                        color: Color(0xff09FFD6),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ]),
+                ),
+                const SizedBox(width: 10),
+                Expanded(
+                  child: Row(children: [
+                    const Icon(
+                      Icons.handshake,
+                      size: 32,
+                      color: Color.fromARGB(255, 177, 177, 177),
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      "${widget.gameCubit.state.draws} draws",
+                      style: const TextStyle(
+                        fontSize: 14,
+                        decoration: TextDecoration.none,
+                        color: Color(0xff8D8D8D),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ]),
+                )
+              ]),
+              const SizedBox(height: 80),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: const [
+                  Button(
+                    text: "PLAY NEW GAME",
+                  ),
+                  SizedBox(height: 15),
+                  Button(
+                    text: "BACK TO HOME",
+                    variant: Variant.secondary,
+                  ),
+                ],
+              ),
             ],
           ),
-        ),
+        )
       ],
     );
   }
